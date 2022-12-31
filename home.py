f2d822756c7fce7ec6af4e1c7edf70fb5ea4c784
Arthur Azevedo <artuenrick@gmail.com>
2022-12-31 17:39:21 -0300

    Test Interface

--- home.py
+++ home.py
@@ -3,11 +3,79 @@ import random as rd
 import string as st
 import re
 
+#For better readability, we put the functions and controls before the return with "view"
 def pg():
     """
     Creates a specific page to use with the "view", in this case, the main page or "home".
     """
-    #For better readability, we put the functions and controls before the return with "view"
+
+    #Characteres types
+    #Based on table ascii 8 bits
+    lowercase = lambda: chr(rd.randint(97, 122))
+    number = lambda: chr(rd.randint(48,57))
+    #These types will be optional
+    uppercase = lambda: chr(rd.randint(65, 90))
+    specials_chars = lambda: chr(rd.randint(33, 47))
+
+    #Habilit more types to characters
+    habilit_upper = True
+    habilit_special = True
+
+    #Random string features
+    size = 20
+    psswd = ""
+
+    #Decision of the types of characters
+    choose_new_char = lambda: rd.randint(1,4)
+
+    #Finally generate string
+    for generate in range(size):
+        decide = choose_new_char()
+        if decide == 1:
+            psswd += lowercase()
+
+        elif decide == 2:
+            psswd += number()
+
+        elif decide == 3:
+            psswd += uppercase()
+
+        elif decide == 4:
+            psswd += specials_chars()
+
+   
+    texto = ft.Text("uepa")
+    def change_size(e):
+        texto.value = f"Oi {select_size.value}"
+        pg.update()
+       
+   
+    select_size = ft.Slider(
+                        on_change = change_size,
+                        max = 100,
+                        min = 0,
+                        divisions=100,
+                        label="{value}"
+                        )
+
+
+    base = ft.ResponsiveRow(
+        [
+            ft.Column(
+                [
+                  ft.Container(
+                    select_size
+                  ),
+                  ft.Container(
+                    texto
+                  )
+                ]
+            )
+        ]
+    )
+
+
+
 
     container_hello = ft.Container(ft.Text("Olá! Você está na home."))
     go_to_help = ft.ElevatedButton(
@@ -20,42 +88,7 @@ def pg():
         scroll = ft.ScrollMode.AUTO,
         controls= [
             container_hello,
-            go_to_help
+            go_to_help,
+            base
         ]
     )
-
-#Characteres types
-#Based on table ascii 8 bits
-lowercase = lambda: chr(rd.randint(97, 122))
-number = lambda: chr(rd.randint(48,57))
-#These types will be optional
-uppercase = lambda: chr(rd.randint(65, 90))
-specials_chars = lambda: chr(rd.randint(33, 47))
-
-#Habilit more types to characters
-habilit_upper = True
-habilit_special = True
-
-#Random string features
-size = 20
-psswd = ""
-
-#Decision of the types of characters
-choose_new_char = lambda: rd.randint(1,4)
-
-#Finally generate string
-for generate in range(size):
-    decide = choose_new_char()
-    if decide == 1:
-        psswd += lowercase()
-
-    elif decide == 2:
-        psswd += number()
-
-    elif decide == 3:
-        psswd += uppercase()
-
-    elif decide == 4:
-        psswd += specials_chars()
-
-print(psswd)
